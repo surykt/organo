8ff6ce96de0247da78dca97af3b205c7f8e965ef
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-03-10 17:04:25 -0300

    initial commit and add banner component and style

---
+++ organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./Components/Banner/Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="banner">
+        <Banner />
+      </header>
+    </div>
+  );
+}
+
+export default App;
