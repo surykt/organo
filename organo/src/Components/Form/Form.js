c2937682a70162a37b46f7e08a3549356f54cccf
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-03-14 18:50:35 -0300

    add dropdown list component to form and key property

--- organo/src/Components/Form/Form.js
+++ organo/src/Components/Form/Form.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Form.css";
 import TextField from "../TextField/TextField";
+import DropdownList from "../DropdownList/DropdownList";
 
 export default function () {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Designer",
+    "Mobile",
+    "Inovação Gestão",
+  ];
+
   return (
     <section className="form">
       <form>
@@ -10,6 +21,7 @@ export default function () {
         <TextField label="Nome" placeholder="Digite seu nome" />
         <TextField label="Cargo" placeholder="Digite seu cargo" />
         <TextField label="Imagem" placeholder="Informe o endereço da imagem" />
+        <DropdownList items={times} />
       </form>
     </section>
   );
