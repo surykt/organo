a863548759551bfc8a89e0ab07326aa9403cc47e
Carlos Alan <cardosocarlosalan@gmail.com>
2023-03-23 22:38:01 -0300

    change button component to react trash icon

--- organo/src/Components/Employee/Employee.js
+++ organo/src/Components/Employee/Employee.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { FaTrashAlt } from 'react-icons/fa'
 import "./Employee.css";
 
 export default function Employee({ name, role, image, deleteEmployee}) {
   return (
     <div className="employee">
+      <FaTrashAlt className="delete-btn" onClick={deleteEmployee}/>
       <div className="header">
         <img src={image} alt={name}></img>
       </div>
       <div className="footer-card">
         <h4>{name}</h4>
         <h5>{role}</h5>
-        <button className="delete-btn" onClick={deleteEmployee}>Deletar Card</button>
       </div>
     </div>
   );
