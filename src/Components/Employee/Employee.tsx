ef614c1f1ad75fffbee2232ec991c5b9a7f6301b
Carlos Alan <cardosocarlosalan@gmail.com>
2023-07-04 15:34:59 -0300

    use Icolaborador in employee component scope

--- src/Components/Employee/Employee.tsx
+++ src/Components/Employee/Employee.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import "./Employee.css";
+import { IColaborador } from "../../shared/IColaborador";
+
+interface EmployeeProps {
+  employee: IColaborador
+  backgroundColor: string
+  deleteEmployee: (id: number) => void
+  favoriteEmployee: (id: number) => void
+}
 
 export default function Employee({
   employee,
   backgroundColor,
   deleteEmployee,
   favoriteEmployee,
-}) {
+} : EmployeeProps) {
   return (
     <div className="employee">
       <FaTrashAlt
@@ -22,7 +30,7 @@ export default function Employee({
         <h4>{employee.name}</h4>
         <h5>{employee.role}</h5>
         <div className="favorite" onClick={() => favoriteEmployee(employee.id)}>
-          {employee.isfavorite ? <AiFillHeart /> : <AiOutlineHeart />}
+          {employee.isFavorite ? <AiFillHeart /> : <AiOutlineHeart />}
         </div>
       </div>
     </div>
